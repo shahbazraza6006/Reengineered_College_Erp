dd14842cdc3c1004942d97a6eccc9d448982a602
rehan-br <95518630+rehan-br@users.noreply.github.com>
2024-01-09 00:21:58 +0500

    feedback implementation complete. Route added

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -5,6 +5,7 @@ import {
   updateStudent,
   testResult,
   attendance,
+  giveFeedback,
 } from "../controller/studentController.js";
 import auth from "../middleware/auth.js";
 
@@ -15,5 +16,6 @@ router.post("/updatepassword", auth, updatedPassword);
 router.post("/updateprofile", auth, updateStudent);
 router.post("/testresult", auth, testResult);
 router.post("/attendance", auth, attendance);
+router.post("/feedback", auth, giveFeedback);
 
 export default router;
